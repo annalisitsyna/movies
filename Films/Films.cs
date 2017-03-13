8adbfc01f0a8336d01cde5510d27ec2389560f54
Anna Lisitsyna <annafox05@gmail.com>
2017-03-13 12:52:08 +0300

    классы

---
+++ Films/Films.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Films
+{
+    class Films
+    {
+        public string Name { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Description { get; set; }
+
+    }
+
+
+}
