9bed11305aacce03b985aa9443688d0847b57eca
Anna Lisitsyna <annafox05@gmail.com>
2017-03-26 22:08:33 +0300

    Логин и пароль в классах

---
+++ Films/Login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Films
+{
+    class Login
+    {
+        public class Films
+        {
+            private string _username;
+
+            public string Userame
+            {
+                get { return _username; }
+                set { _username = value; }
+            }
+            private string _password;
+            public string Password
+            {
+                get { return _password; }
+                set { _password = value; }
+            }
+
+            public Films(string username, string password)
+            {
+                _username = username;
+                _password = password;
+                
+            }
+        }
+
+    }
+}
